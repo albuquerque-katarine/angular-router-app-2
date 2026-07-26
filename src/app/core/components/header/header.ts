f1ce83104d502e369a20b0688db5f5d81c4728d8
albuquerque-katarine <kba.2879@gmail.com>
2026-07-26 18:53:08 -0300

    Orrganização e criação do componente Navbar.

--- src/app/core/components/header/header.ts
+++ src/app/core/components/header/header.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { Navbar } from "../navbar/navbar";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [Navbar],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
--- src/app/core/pages/home/home.ts
+++ src/app/core/pages/home/home.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { Content } from "../../components/content/content";
-import { Header } from "../../components/header/header";
-import { Footer } from "../../components/footer/footer";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Content, Header, Footer],
+  imports: [Content],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
