7f6b509c9394ef8c12f34f4ed2cc20992ea68411
albuquerque-katarine <kba.2879@gmail.com>
2026-07-26 18:33:05 -0300

    Criação do componente Footer.

--- src/app/core/pages/home/home.ts
+++ src/app/core/pages/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Content } from "../../components/content/content";
 import { Header } from "../../components/header/header";
+import { Footer } from "../../components/footer/footer";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Content, Header],
+  imports: [Content, Header, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
